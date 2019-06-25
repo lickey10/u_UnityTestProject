bd88985e3bd42f1d22d491287c909146b510cf8f
lickey10 <lickey10@gmail.com>
2019-06-24 21:49:10 -0600

    first import

---
+++ unityTestProject/New Unity test Project/Assets/AndroidPhotoGalleryPlugin/scripts/AndroidHelperClasses/AndroidPhotoGalleryHelperClass.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidPhotoGalleryHelperClass : MonoBehaviour {
+
+	private static AndroidPhotoGalleryHelperClass _instance;
+	string theError = "";
+	string theError2 = "";
+	public AndroidJavaObject AndroidPhotoGallery;
+
+	private AndroidJavaObject testobj = null;
+	private AndroidJavaObject playerActivityContext = null;
+
+	public static AndroidPhotoGalleryHelperClass Instance
+	{
+		get
+		{
+			if(_instance == null)
+				_instance = new AndroidPhotoGalleryHelperClass();
+			
+			return _instance;
+		}
+	}
+	
+	public AndroidPhotoGalleryHelperClass()
+	{
+		AndroidPhotoGallery = new AndroidJavaObject ("com.snicklefritz.utility.AndroidPhotoGallery");
+
+		// First, obtain the current activity context
+		using (var actClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+			playerActivityContext = actClass.GetStatic<AndroidJavaObject>("currentActivity");
+		}
+
+		AndroidPhotoGallery.Call("SetCurrentActivity",playerActivityContext);
+	}
+	
+	public void Open()
+	{
+		AndroidPhotoGallery.Call("Open");
+	}
+
+	public void Insert(string ImagePath)
+	{
+		AndroidPhotoGallery.Call("Insert", ImagePath);
+	}
+	
+	void OnApplicationQuit()
+	{
+		_instance = null;
+	}
+}
---
+++ unityTestProject/New Unity test Project/Assets/AndroidPhotoGalleryPlugin/scripts/AndroidPhotoGallery.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class AndroidPhotoGallery : MonoBehaviour {
+	private AndroidPhotoGalleryHelperClass androidPhotoGalleryHelperClass;
+
+	// Use this for initialization
+	void Start () {
+		androidPhotoGalleryHelperClass = AndroidPhotoGalleryHelperClass.Instance;
+	}
+
+	public void Open()
+	{
+		androidPhotoGalleryHelperClass.Open();
+	}
+}
+
